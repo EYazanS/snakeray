6ed93e39b99d46b78e068c651adfcec8fbc197ef
Yazan Salameh <yazan@serenbit.ps>
2024-08-24 23:29:12 +0300

    Create game structs, draw food and snake parts

--- src/game.h
+++ src/game.h
@@ -1,15 +1,32 @@
 #ifndef game_h
 #define game_h
 
+#include <raylib.h>
+
+#define GridSize 1024
 
 typedef struct {
+    Vector2 Pieces[GridSize];
+    int Size;
+} Snake;
 
+typedef struct {
+    AudioStream Audio[2];
+    Texture2D FoodTexture;
+    Vector2 FoodPosition;
+    Snake Snake;
+    Vector2 Grid;
 } GameState;
 
 typedef struct {
     int ScreenWidth;
     int ScreenHeight;
     int Margin;
+    int SnakeBlockSize;
+    int TileWidth;
+    int TileHeight;
+    int GridWidth;
+    int GridHeight;
 } Constants;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -8,6 +8,13 @@ int main(void) {
     constants.ScreenWidth = 800;
     constants.ScreenHeight = 450;
     constants.Margin = 10;
+    constants.SnakeBlockSize = 20;
+
+    constants.GridWidth = 50;
+    constants.GridHeight = 50;
+
+    constants.TileWidth = constants.ScreenWidth / constants.GridWidth;
+    constants.TileHeight = constants.ScreenHeight / constants.GridHeight;
 
     GameState game_state = {};
 
@@ -15,17 +22,35 @@ int main(void) {
 
     SetTargetFPS(60);
 
-    Vector2 ball_new_location;
+    Image foodImage = LoadImage("assets/food.png");
+
+    game_state.FoodTexture = LoadTextureFromImage(foodImage);  // Image converted to texture, GPU memory (VRAM);
+
+    Snake* snake = &game_state.Snake;
+
+    snake->Size = 1;
+    snake->Pieces[0] = (Vector2){5, 5};
+
+    game_state.FoodPosition = (Vector2){25, 25};
 
     while (!WindowShouldClose()) {
         // Update
-      
+
         // Draw
         BeginDrawing();
 
         ClearBackground(RAYWHITE);
 
         DrawRectangle(0, 0, constants.ScreenWidth, constants.ScreenHeight, DARKGRAY);
+
+        DrawTexture(game_state.FoodTexture,
+                    (game_state.FoodPosition.x - game_state.FoodTexture.width / 2) * constants.TileWidth,
+                    (game_state.FoodPosition.y - game_state.FoodTexture.height / 2) * constants.TileHeight, WHITE);
+
+        for (int i = 0; i < snake->Size; i++) {
+            DrawRectangle(snake->Pieces[i].x * constants.TileWidth, snake->Pieces[i].y * constants.TileHeight,
+                          constants.SnakeBlockSize, constants.SnakeBlockSize, GREEN);
+        }
 
         EndDrawing();
     }
