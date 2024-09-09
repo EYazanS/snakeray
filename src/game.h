b3e6246ce49ac1c8795361a9bfeaa8d8ab2741c9
Yazan Salameh <yazan@serenbit.ps>
2024-09-09 15:43:59 +0300

    Added game mode enum

--- src/game.h
+++ src/game.h
@@ -5,28 +5,31 @@
 
 #define GridSize 1024
 
+typedef enum { MAIN_MENU, PLAYING, GAME_OVER } GameMode;
+
 typedef struct {
-    Vector2 Pieces[GridSize];
-    Vector2 MovementDirection;
-    int Size;
+  Vector2 Pieces[GridSize];
+  Vector2 MovementDirection;
+  int Size;
 } Snake;
 
 typedef struct {
-    Music Audio[2];
-    Texture2D FoodTexture;
-    Vector2 FoodPosition;
-    Snake Snake;
+  Music Audio[2];
+  Texture2D FoodTexture;
+  Vector2 FoodPosition;
+  Snake Snake;
+  GameMode Mode;
 } GameState;
 
 typedef struct {
-    int ScreenWidth;
-    int ScreenHeight;
-    int Margin;
-    int SnakeBlockSize;
-    int TileWidth;
-    int TileHeight;
-    int GridWidth;
-    int GridHeight;
+  int ScreenWidth;
+  int ScreenHeight;
+  int Margin;
+  int SnakeBlockSize;
+  int TileWidth;
+  int TileHeight;
+  int GridWidth;
+  int GridHeight;
 } Constants;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -3,147 +3,162 @@
 #include "game.h"
 
 int main(void) {
-    // Initialization
-    Constants constants = {};
-    constants.ScreenWidth = 800;
-    constants.ScreenHeight = 800;
-    constants.Margin = 10;
-    constants.SnakeBlockSize = 20;
+  // Initialization
+  Constants constants = {};
 
-    constants.GridWidth = 50;
-    constants.GridHeight = 50;
+  constants.ScreenWidth = 800;
+  constants.ScreenHeight = 800;
+  constants.Margin = 10;
+  constants.SnakeBlockSize = 20;
 
-    constants.TileWidth = constants.ScreenWidth / constants.GridWidth;
-    constants.TileHeight = constants.ScreenHeight / constants.GridHeight;
+  constants.GridWidth = 50;
+  constants.GridHeight = 50;
 
-    GameState game_state = {};
+  constants.TileWidth = constants.ScreenWidth / constants.GridWidth;
+  constants.TileHeight = constants.ScreenHeight / constants.GridHeight;
 
-    InitWindow(constants.ScreenWidth, constants.ScreenHeight, "Snakrray");
+  GameState game_state = {};
 
-    InitAudioDevice();  // Initialize audio device
+  InitWindow(constants.ScreenWidth, constants.ScreenHeight, "Snakrray");
 
-    SetTargetFPS(60);
+  InitAudioDevice(); // Initialize audio device
 
-    Image foodImage = LoadImage("assets/food.png");
+  SetTargetFPS(60);
 
-    ImageResize(&foodImage, constants.TileWidth, constants.TileHeight);
+  Image foodImage = LoadImage("assets/food.png");
 
-    game_state.FoodTexture = LoadTextureFromImage(foodImage);  // Image converted to texture, GPU memory (VRAM);
-    game_state.Audio[0] = LoadMusicStream("assets/eat.mp3");
-    game_state.Audio[1] = LoadMusicStream("assets/wall.mp3");
-    game_state.Audio[0].looping = false;
-    game_state.Audio[1].looping = false;
+  ImageResize(&foodImage, constants.TileWidth, constants.TileHeight);
 
-    Snake* snake = &game_state.Snake;
+  game_state.FoodTexture = LoadTextureFromImage(
+      foodImage); // Image converted to texture, GPU memory (VRAM);
+  game_state.Audio[0] = LoadMusicStream("assets/eat.mp3");
+  game_state.Audio[1] = LoadMusicStream("assets/wall.mp3");
+  game_state.Audio[0].looping = false;
+  game_state.Audio[1].looping = false;
 
-    snake->Size = 3;
-    snake->Pieces[0] = (Vector2){5, 5};
-    snake->Pieces[1] = (Vector2){4, 5};
-    snake->Pieces[2] = (Vector2){3, 5};
-    snake->MovementDirection = (Vector2){1, 0};
+  Snake *snake = &game_state.Snake;
 
-    game_state.FoodPosition = (Vector2){constants.GridWidth / 2, constants.GridHeight / 2};
+  snake->Size = 3;
+  snake->Pieces[0] = (Vector2){5, 5};
+  snake->Pieces[1] = (Vector2){4, 5};
+  snake->Pieces[2] = (Vector2){3, 5};
+  snake->MovementDirection = (Vector2){1, 0};
 
-    unsigned long frameCounter = 0;
+  game_state.FoodPosition =
+      (Vector2){constants.GridWidth / 2.0f, constants.GridHeight / 2.0f};
 
-    while (!WindowShouldClose()) {
-        // Update
-        frameCounter++;
+  unsigned long frameCounter = 0;
 
-        UpdateMusicStream(game_state.Audio[0]);
-        UpdateMusicStream(game_state.Audio[1]);
+  game_state.Mode = MAIN_MENU;
 
-        if (IsKeyDown(KEY_W) && snake->MovementDirection.y != 1) {
-            snake->MovementDirection = (Vector2){0, -1};
-        }
+  while (!WindowShouldClose()) {
+    // Update
+    frameCounter++;
 
-        if (IsKeyDown(KEY_S) && snake->MovementDirection.y != -1) {
-            snake->MovementDirection = (Vector2){0, 1};
-        }
+    UpdateMusicStream(game_state.Audio[0]);
+    UpdateMusicStream(game_state.Audio[1]);
 
-        if (IsKeyDown(KEY_A) && snake->MovementDirection.x != 1) {
-            snake->MovementDirection = (Vector2){-1, 0};
-        }
-
-        if (IsKeyDown(KEY_D) && snake->MovementDirection.x != -1) {
-            snake->MovementDirection = (Vector2){1, 0};
-        }
-
-        if (frameCounter % 5 == 0) {
-            // Move snake every 20 frame?
-            for (int i = snake->Size - 1; i >= 1; i--) {
-                snake->Pieces[i].x = snake->Pieces[i - 1].x;
-                snake->Pieces[i].y = snake->Pieces[i - 1].y;
-            }
-
-            snake->Pieces[0].x += snake->MovementDirection.x;
-            snake->Pieces[0].y += snake->MovementDirection.y;
-        }
-
-        if (snake->Pieces[0].x == game_state.FoodPosition.x && snake->Pieces[0].y == game_state.FoodPosition.y) {
-            snake->Pieces[snake->Size].x = snake->Pieces[snake->Size - 1].x;
-            snake->Pieces[snake->Size].y = snake->Pieces[snake->Size - 1].y;
-            snake->Size++;
-
-            PlayMusicStream(game_state.Audio[0]);
-
-            int new_valid_position = 1;
-
-            Vector2 new_position = (Vector2){0, 0};
-
-            do {
-                new_valid_position = 1;
-
-                new_position.x = GetRandomValue(1, constants.GridWidth - 1);
-                new_position.y = GetRandomValue(1, constants.GridHeight - 1);
-
-                for (int i = snake->Size - 1; i >= 1; i--) {
-                    if (snake->Pieces[i].x == new_position.x && snake->Pieces[i].y == new_position.y) {
-                        new_valid_position = 0;
-                    }
-                }
-            } while (!new_valid_position);
-
-            game_state.FoodPosition = new_position;
-        }
-
-        if (snake->Pieces[0].x < 0 || snake->Pieces[0].y < 0 || snake->Pieces[0].x == constants.GridWidth ||
-            snake->Pieces[0].y == constants.GridHeight) {
-            // TODO: Gameover
-            PlayMusicStream(game_state.Audio[1]);
-        }
-
-        for (int i = snake->Size - 1; i >= 1; i--) {
-            if (snake->Pieces[i].x == snake->Pieces[0].x && snake->Pieces[i].y == snake->Pieces[0].y) {
-                // TODO: Gameover
-            }
-        }
-
-        // Draw
-        BeginDrawing();
-
-        ClearBackground(RAYWHITE);
-
-        DrawRectangle(0, 0, constants.ScreenWidth, constants.ScreenHeight, DARKGRAY);
-
-        DrawRectangle(snake->Pieces[0].x * constants.TileWidth, snake->Pieces[0].y * constants.TileHeight,
-                      constants.SnakeBlockSize, constants.SnakeBlockSize, DARKGREEN);
-
-        for (int i = 1; i < snake->Size; i++) {
-            DrawRectangle(snake->Pieces[i].x * constants.TileWidth, snake->Pieces[i].y * constants.TileHeight,
-                          constants.SnakeBlockSize, constants.SnakeBlockSize, GREEN);
-        }
-
-        DrawTexture(game_state.FoodTexture, game_state.FoodPosition.x * constants.TileWidth,
-                    game_state.FoodPosition.y * constants.TileHeight, WHITE);
-
-        EndDrawing();
+    if (IsKeyDown(KEY_W) && snake->MovementDirection.y != 1) {
+      snake->MovementDirection = (Vector2){0, -1};
     }
 
-    // De-Initialization
-    CloseAudioDevice();  // Close audio device (music streaming is automatically stopped)
+    if (IsKeyDown(KEY_S) && snake->MovementDirection.y != -1) {
+      snake->MovementDirection = (Vector2){0, 1};
+    }
 
-    CloseWindow();
+    if (IsKeyDown(KEY_A) && snake->MovementDirection.x != 1) {
+      snake->MovementDirection = (Vector2){-1, 0};
+    }
 
-    return 0;
+    if (IsKeyDown(KEY_D) && snake->MovementDirection.x != -1) {
+      snake->MovementDirection = (Vector2){1, 0};
+    }
+
+    if (frameCounter % 5 == 0) {
+      // Move snake every 20 frame?
+      for (int i = snake->Size - 1; i >= 1; i--) {
+        snake->Pieces[i].x = snake->Pieces[i - 1].x;
+        snake->Pieces[i].y = snake->Pieces[i - 1].y;
+      }
+
+      snake->Pieces[0].x += snake->MovementDirection.x;
+      snake->Pieces[0].y += snake->MovementDirection.y;
+    }
+
+    if (snake->Pieces[0].x == game_state.FoodPosition.x &&
+        snake->Pieces[0].y == game_state.FoodPosition.y) {
+      snake->Pieces[snake->Size].x = snake->Pieces[snake->Size - 1].x;
+      snake->Pieces[snake->Size].y = snake->Pieces[snake->Size - 1].y;
+      snake->Size++;
+
+      PlayMusicStream(game_state.Audio[0]);
+
+      int new_valid_position = 1;
+
+      Vector2 new_position = (Vector2){0, 0};
+
+      do {
+        new_valid_position = 1;
+
+        new_position.x = GetRandomValue(1, constants.GridWidth - 1);
+        new_position.y = GetRandomValue(1, constants.GridHeight - 1);
+
+        for (int i = snake->Size - 1; i >= 1; i--) {
+          if (snake->Pieces[i].x == new_position.x &&
+              snake->Pieces[i].y == new_position.y) {
+            new_valid_position = 0;
+          }
+        }
+      } while (!new_valid_position);
+
+      game_state.FoodPosition = new_position;
+    }
+
+    if (snake->Pieces[0].x < 0 || snake->Pieces[0].y < 0 ||
+        snake->Pieces[0].x == constants.GridWidth ||
+        snake->Pieces[0].y == constants.GridHeight) {
+      // TODO: Gameover
+      PlayMusicStream(game_state.Audio[1]);
+    }
+
+    for (int i = snake->Size - 1; i >= 1; i--) {
+      if (snake->Pieces[i].x == snake->Pieces[0].x &&
+          snake->Pieces[i].y == snake->Pieces[0].y) {
+        // TODO: Gameover
+      }
+    }
+
+    // Draw
+    BeginDrawing();
+
+    ClearBackground(RAYWHITE);
+
+    DrawRectangle(0, 0, constants.ScreenWidth, constants.ScreenHeight,
+                  DARKGRAY);
+
+    DrawRectangle(snake->Pieces[0].x * constants.TileWidth,
+                  snake->Pieces[0].y * constants.TileHeight,
+                  constants.SnakeBlockSize, constants.SnakeBlockSize,
+                  DARKGREEN);
+
+    for (int i = 1; i < snake->Size; i++) {
+      DrawRectangle(snake->Pieces[i].x * constants.TileWidth,
+                    snake->Pieces[i].y * constants.TileHeight,
+                    constants.SnakeBlockSize, constants.SnakeBlockSize, GREEN);
+    }
+
+    DrawTexture(game_state.FoodTexture,
+                game_state.FoodPosition.x * constants.TileWidth,
+                game_state.FoodPosition.y * constants.TileHeight, WHITE);
+
+    EndDrawing();
+  }
+
+  // De-Initialization
+  CloseAudioDevice(); // Close audio device (music streaming is automatically
+                      // stopped)
+
+  CloseWindow();
+
+  return 0;
 }
